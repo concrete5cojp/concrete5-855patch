466355a7ffb726e38b05b4a147b98163245de70f
Katz Ueno <katz@concrete5.co.jp>
2021-07-13 09:58:14 +0900

    Before Dropzone.js cache included tokens thus concrete5 generated cache forever. This fix solve the issue.

--- application/config/app.php
+++ application/config/app.php
@@ -1,4 +1,16 @@
 <?php
 
 return [
+    /**
+     * Apply patch for dropzone.js not to become cache file
+     * @todo remove this override if it's fixed in the future.
+     * @link https://github.com/concrete5/concrete5/pull/9510
+     */
+    'assets' => [
+        'dropzone' => [
+            ['javascript', 'js/dropzone.js'],
+            ['javascript-localized', '/ccm/assets/localization/dropzone/js', ['combine' => false]],
+            ['css', 'css/dropzone.css', ['minify' => false]],
+        ],
+    ]
 ];
